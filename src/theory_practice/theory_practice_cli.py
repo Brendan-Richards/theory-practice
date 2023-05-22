623f3e9c8b268a942e78a8c14647f33fe6845f02
quend00@gmail.com <quend00@gmail.com>
2023-05-21 20:41:23 -0400

    implement triad mode

--- src/theory_practice/theory_practice_cli.py
+++ src/theory_practice/theory_practice_cli.py
@@ -3,7 +3,7 @@ import random
 import os
 import toml
 
-from constants import CHROMATICS, INTERVALS, CONFIG_DIRS, THEORY_MODES
+from constants import CHROMATICS, INTERVALS, CONFIG_DIRS, THEORY_MODES, TRIAD_FORMULAS
 
 
 class TheoryTeacher:
@@ -95,6 +95,28 @@ class TheoryTeacher:
         elif self.mode == "Chord Spelling":
             # TODO: implement chord mode config validation
             pass
+        elif self.mode == "Guitar Triads":
+            expected_options = {"low_strings", "roots", "chord_types", "inversions"}
+            if not set(config.keys()) == expected_options:
+                raise ValueError(
+                    f"Expected config to have the following options only: {expected_options}"
+                )
+            if set(config["low_strings"]) - {"e", "a", "d", "g"}:
+                raise ValueError("Unexpected low strings found in config")
+            if set(config["roots"]) - set([y for x in CHROMATICS for y in x]):
+                if (
+                    config["roots"] != ["naturals"]
+                    and config["roots"] != ["accidentals"]
+                    and config["roots"] != ["flats"]
+                    and config["roots"] != ["sharps"]
+                    and config["roots"] != []
+                    or len(config["roots"]) > 1
+                ):
+                    raise ValueError("Unexpected roots found in config")
+            if set(config["chord_types"]) - {"m", "M", "dim", "aug"}:
+                raise ValueError("Unexpected chord types found in config")
+            if set(config["inversions"]) - {0, 1, 2}:
+                raise ValueError("Unexpected inversions found in config")
 
         return config
 
@@ -114,6 +136,60 @@ class TheoryTeacher:
             return [x for x in possible_notes if "#" in x]
         else:
             return roots
+
+    def get_chord_tab(
+        self, low_string: str, inversion: int, chord_type: str, root_idx: int
+    ) -> str:
+        formula = TRIAD_FORMULAS[f"{chord_type}_{low_string}_{inversion}"]
+
+        if inversion == 0:
+            lowest_note = CHROMATICS[root_idx][0]
+        elif inversion == 1:
+            if chord_type in ["m", "dim"]:
+                lowest_note = CHROMATICS[(root_idx + 3) % 12][0]
+            elif chord_type in ["M", "aug"]:
+                lowest_note = CHROMATICS[(root_idx + 4) % 12][0]
+        elif inversion == 2:
+            lowest_note = CHROMATICS[(root_idx + 7) % 12][0]
+        else:
+            raise ValueError(f"Inversion num: {inversion} not supported!")
+        starting_fret_num = self.distance(low_string.upper(), lowest_note.upper())
+
+        # if any chord note would put us into negative frets, just transpose the whole chord up an octave
+        for point in formula:
+            if starting_fret_num - point[1] < 0:
+                starting_fret_num += 12
+                break
+
+        string_chunks = []
+        point = 0
+        for string in ["E", "A", "D", "G", "B", "E"]:
+            if string == low_string.upper() and point < len(formula):
+                string_chunks.append(f"{string} -{starting_fret_num}-\n")
+                point = 1
+            elif point == 0 or point >= len(formula):
+                string_chunks.append(f"{string} ---\n")
+            else:
+                y = formula[point][1]
+                string_chunks.append(f"{string} -{starting_fret_num - y}-\n")
+                point += 1
+
+        string_chunks = string_chunks[::-1]
+        chord_tab = "".join(string_chunks)
+        return chord_tab
+
+    def distance(self, n1: str, n2: str) -> int:
+        idx1 = [
+            i
+            for i, note_list in enumerate(CHROMATICS)
+            for note in note_list
+            if note == n1
+        ][0]
+
+        for i in range(idx1, idx1 + 12):
+            for note in CHROMATICS[i % 12]:
+                if note == n2:
+                    return i - idx1
 
     def run_cli(self):
         quit = False
@@ -153,8 +229,26 @@ class TheoryTeacher:
 
             elif self.mode == "Chord Spelling":
                 raise NotImplementedError(f"theory mode: {self.mode} not implemented!")
+
             elif self.mode == "Guitar Triads":
-                raise NotImplementedError(f"theory mode: {self.mode} not implemented!")
+                root_id = random.choice(self.root_ids)
+                root = random.choice(
+                    [x for x in CHROMATICS[root_id] if x in self.roots]
+                )
+                low_string = random.choice(self.config["low_strings"])
+                inversion = random.choice(self.config["inversions"])
+                chord_type = random.choice(self.config["chord_types"])
+                chord_tab = self.get_chord_tab(
+                    low_string, inversion, chord_type, root_id
+                )
+                input(
+                    f"Low String: {low_string.upper()} \nInversion: {inversion}\nChord Name: {root + chord_type}\n\nhit enter for answer \n"
+                )
+
+                print(f"\n{chord_tab}\n")
+                inp = input("hit enter to continue \n")
+                os.system("cls" if os.name == "nt" else "clear")
+
             else:
                 raise NotImplementedError(f"theory mode: {self.mode} not implemented!")
 
